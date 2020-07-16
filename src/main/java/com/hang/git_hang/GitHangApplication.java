e3ed541319482021f291f4eeb6c386fc162e149d
liuwh0715 <liuweihang0715@163.com>
2020-07-16 21:39:09 +0800

    提交

---
+++ src/main/java/com/hang/git_hang/GitHangApplication.java
@@ -0,0 +1,13 @@
+package com.hang.git_hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHangApplication.class, args);
+    }
+
+}
